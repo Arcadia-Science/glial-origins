7af42b59e7f0b02b0c3e822ec8d0f490b5560bcc
Dennis August Sun <dennis.a.sun@gmail.com>
2022-10-24 10:51:53 -0700

    hotfix to allow unpickling from s3
    Signed-off-by: Dennis August Sun <dennis.a.sun@gmail.com>

--- utils/biofile_handling.py
+++ utils/biofile_handling.py
@@ -400,6 +400,12 @@ class IdmmFile(BioFile):
         self.sources = sources
         self.kind = kind
 
+class CellAnnotFile(BioFile):
+    def __init__(self, filename, sampledict, sources: list, **kwargs):
+        super().__init__(filename = filename, sampledict = sampledict, **kwargs)
+        self.s3uri = 's3://arcadia-reference-datasets/organisms/' + self.species + '/functional_sequencing/scRNA-Seq/' + self.filename
+        self.sources = sources
+
 class UniprotIDMapperFile(IdmmFile):
     def __init__(self, filename, sampledict, kind, sources: list, from_type, to_type, **kwargs):
         super().__init__(filename = filename, sampledict = sampledict, kind = kind, sources = sources, **kwargs)
